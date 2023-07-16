ff5207521984142d0d3b5ba51803ba96cef17825
alexmb356 <alexmb356@hotmail.com>
2023-07-15 23:33:06 -0300

    se agrega un nuevo login

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import Perfil from './componentes/auth/Perfil';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import Navigation from "./componentes/Navigation";
-import LogIn from './componentes/auth/LogIn';
+import LogIns from './componentes/auth/LogIns';
 import Admin from './componentes/auth/Admin';
 import Registrarse from './componentes/auth/Registrarse';
 import Mostrar from './componentes/auth/Mostrar';
@@ -81,7 +81,7 @@ function App() {
                 <Route index element={<HomePage/>}/>
                 <Route path='movie/:id' element={<MoviePage/>}/>
                 <Route path="search" element={<SearchPage/>} />
-                <Route path='/Login' element={<LogIn/>}/>
+                <Route path='/Login' element={<LogIns/>}/>
                 <Route path='/Admin' element={<Admin/>}/>
                 <Route path='/Mostrar' element={<Mostrar/>}/>
                 <Route path='/editarusuario/:id' element={<Editar/>}/>
